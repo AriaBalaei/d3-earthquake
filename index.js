d71fa7aef78de5204c62c9c3e3eef85caa618e28
Aria Balaei <ariabalayi@gmail.com>
2023-08-17 22:11:41 +0200

    Data and json was added

---
+++ index.js
@@ -0,0 +1,23 @@
+const canvas = d3.select('.canva');
+
+let width = '100%';
+let height = '100%';
+const api_url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson';
+//const api_url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson';
+const svg = canvas.append('svg')
+  .attr('width', width)
+  .attr('height', height)
+
+//JSON
+d3.json(api_url).then(data => {
+  console.log(data)
+    const circle = svg.selectAll('circle')
+        .data(data.features);
+  
+    circle.enter().append('circle')    
+      .attr('cx', (d, i) => Math.floor(Math.random() * 450) + (d.properties.mag * 2))
+      .attr('cy', (d, i) => Math.floor(Math.random() * 100) + d.properties.mag + 4)
+      .attr('r', (d, i) => (d.properties.mag)*2)  
+      .attr('fill', (d, i) => d.properties.alert)  
+ 
+})
