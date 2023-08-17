1d58361a2b55794e8d7bde065096a17be16aef42
Aria Balaei <ariabalayi@gmail.com>
2023-08-17 23:00:14 +0200

    Mouseover added

--- index.js
+++ index.js
@@ -18,6 +18,24 @@ d3.json(api_url).then(data => {
       .attr('cx', (d, i) => Math.floor(Math.random() * 450) + (d.properties.mag * 2))
       .attr('cy', (d, i) => Math.floor(Math.random() * 100) + d.properties.mag + 4)
       .attr('r', (d, i) => (d.properties.mag)*2)  
+      .style('top', '156')
+      
+      .on('mouseover', function(event){
+        console.log(event.target);
+         d3.select(event.target)
+         .transition()
+         .duration(100)//100 ms
+         .style('opacity', '0.7')
+         
+      })
+
+      .on('mouseout', function(event){
+        console.log(event.target);
+         d3.select(event.target)
+         .transition()
+         .duration(100)//100 ms
+         .style('opacity', '1')
+      })
       .attr('fill', (d, i) => d.properties.alert)  
  
 })
